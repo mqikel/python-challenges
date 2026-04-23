4ff0b4620feec211e80c38b78b116bdd2523f34c
Victor Carrasco <vc.business.oo@gmail.com>
2026-04-23 21:32:24 +0200

    feat: Se ha añadido un ejercicio de diccionarios

---
+++ ejercicios/basic_dict.py
@@ -0,0 +1,30 @@
+jornada_hoy = {
+    "fecha": "23/04/2026",
+    "equipo_a": "Barcelona",
+    "equibo_b": "Madrid",
+    "goles_a": 2,
+    "goles_b": 1,
+    "es_clasificatorio": False
+}
+jornada_mañana = {
+    "fecha": "24/04/2026",
+    "equipo_a": "Mayorca",
+    "equibo_b": "Atleti",
+    "goles_a": 0,
+    "goles_b": 0,
+    "es_clasificatorio": True
+}
+jornada_ayer = {
+    "fecha": "22/04/2026",
+    "equipo_a": "Malaga",
+    "equibo_b": "Sevilla",
+    "goles_a": 2,
+    "goles_b": 0,
+    "es_clasificatorio": True
+}
+
+jornadas = [jornada_mañana, jornada_hoy, jornada_ayer]
+
+"""
+    Se deben imprimir por pantalla SOLO los partidos que sean clasificatorios
+"""
