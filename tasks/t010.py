ce28d32704f29e5a4d0b5d4e229d4cfcb51fde0e
mqikel <miguelblancoosuna02@gmail.com>
2026-04-24 13:24:35 +0200

    feat: t010 terminada y funcionando con los requisitos pedidos, pero aun sin revisar

--- tasks/t010.py
+++ tasks/t010.py
@@ -59,23 +59,62 @@ Salida esperada del segundo mostrar_cliente:
 """
 
 
-# def crear_cliente(nombre:str, email:str, ciudad:str) -> dict:
-#     ...
+def crear_cliente(nombre:str, email:str, ciudad:str, pedidos:int) -> dict:
+    """
+    Esta función va a recoger la información de cada cliente
+    y la va a convertir en un diccionario
+    """
+    cliente = {
+        "nombre": nombre,
+        "email": email,
+        "ciudad": ciudad,
+        "pedidos": pedidos
+    }
+    return cliente
 
-# def mostrar_cliente(cliente:dict) -> None:
-#     ...
 
-# def actualizar_ciudad(cliente:dict, nueva_ciudad:str) -> dict:
-#     ...
+def mostrar_cliente(cliente:dict) -> None:
+    """
+    Esta función va a mostrar los datos del cliente ya recogidos
+    y transformados en diccionario
+    """
+    print(f"Nombre: {cliente["nombre"]}\n"
+          f"Email: {cliente["email"]}\n"
+          f"Ciudad: {cliente["ciudad"]}\n"
+          f"Pedidos: {cliente["pedidos"]}\n")
+    
 
-# def registrar_pedido(cliente:dict) -> dict:
-#     ...
+def actualizar_ciudad(cliente:dict, nueva_ciudad:str) -> dict:
+    """
+    Esta función la utilizaremos para actualizar los datos de
+    la nuevo ciudad en la que se encuentra el cliente
+    """
+    cliente["ciudad"] = nueva_ciudad
+    return cliente
 
 
+def registrar_pedido(cliente:dict) -> dict:
+    """
+    Con esta función vamos a sumar el numero de pedidos al apartado
+    de pedidos del cliente
+    """
+    cliente_nuevo_pedido = {
+        "nombre": cliente["nombre"],
+        "email": cliente["email"],
+        "ciudad": cliente["ciudad"],
+        "pedidos": cliente["pedidos"]
+    }
+    cliente_nuevo_pedido["pedidos"] = cliente["pedidos"] + 1
+    return cliente_nuevo_pedido
+    
+    
 # --- Pruebas ---
-# cliente = crear_cliente("Ana", "ana@mail.com", "Madrid")
-# mostrar_cliente(cliente)
-# cliente = actualizar_ciudad(cliente, "Valencia")
-# cliente = registrar_pedido(cliente)
-# cliente = registrar_pedido(cliente)
-# mostrar_cliente(cliente)
+cliente = crear_cliente("Ana", "ana@mail.com", "Madrid", 0)
+cliente = actualizar_ciudad(cliente, "Valencia")
+cliente = registrar_pedido(cliente)
+cliente = registrar_pedido(cliente)
+print(mostrar_cliente(cliente))
+
+
+
+
