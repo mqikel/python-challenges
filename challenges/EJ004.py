f94732b5abc66022e3cea1dca1b45374742d8b46
Victor Carrasco <vc.business.oo@gmail.com>
2026-05-20 11:50:43 +0200

    feat: Added exercise 4 and 5

---
+++ challenges/EJ004.py
@@ -0,0 +1,31 @@
+"""
+Un videojuego online necesita validar nombres de usuario antes de registrarlos en el sistema.
+Crea una función que valide los siguientes nombres de usuarios:
+"""
+usuarios = [
+    "TitanGamer7",
+    "IronLiftX9",
+    "ShadowPlayer21",
+    "PixelWarrior5",
+    "MegaHunter88",
+    "AtlasPower4",
+    "CyberKnight12",
+    "AlphaStriker3",
+    "DragonSlayer77",
+    "SpeedRunnerX2",
+    "IronCore99",
+    "NovaFighter6",
+    "SteelChampion1",
+    "RogueSniper44",
+    "TitanForce8",
+    "GamerElite55",
+    "PowerBuilder9",
+    "ThunderStrike22",
+    "CodeMaster11",
+    "DarkPhantom33",
+    "LiftLegend10",
+    "VelocityHero66",
+    "MatrixPlayer7",
+    "QuantumRider2",
+    "SpartanMode5"
+]
---
+++ challenges/EJ005.py
@@ -0,0 +1,7 @@
+"""
+Trabajas como programador en una empresa que desarrolla software para entrenadores de halterofilia. El sistema ya contiene funciones internas que calculan porcentajes y redondean pesos a discos reales.
+
+Tu tarea es crear una función que genere una sesión completa de entrenamiento usando esas funciones ya existentes.
+
+Este escenario es realista: en la práctica muchas veces trabajarás con funciones ya hechas por otros desarrolladores y deberás entenderlas y reutilizarlas.
+"""
